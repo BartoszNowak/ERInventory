af389ce47f1cd87434f0b8ee386a744f57ad1246
Bartek <bartosznowak199605@gmail.com>
2022-07-02 22:04:24 +0200

    Finished autoscroll

--- Assets/Scripts/ScrollRectAutoScroll.cs
+++ Assets/Scripts/ScrollRectAutoScroll.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,72 +7,47 @@ using UnityEngine.UI;
 [RequireComponent(typeof(ScrollRect))]
 public class ScrollRectAutoScroll : MonoBehaviour
 {
-    public float scrollSpeed = 10f;
-    private bool mouseOver = false;
+    [SerializeField]
+    private int _rows = 5;
+    [SerializeField]
+    private int _coulums = 5;
 
-    private List<Selectable> m_Selectables = new List<Selectable>();
-    private ScrollRect m_ScrollRect;
-
-    private Vector2 m_NextScrollPosition = Vector2.up;
-
-    private int rows;
-    private float scrollMoveFraction;
-
-    private int lastSelected;
-    //private int lastRow;
+    private List<Selectable> _selectables = new List<Selectable>();
+    private ScrollRect _scrollRect;
+    private Vector2 _nextScrollPosition = Vector2.up;
+    private int _totalRows;
+    private float _scrollMoveFraction;
+    private int _lastSelectedSlotIndex;
 
     void OnEnable()
     {
-        if (m_ScrollRect)
+        if (_scrollRect)
         {
-            m_ScrollRect.content.GetComponentsInChildren(m_Selectables);
+            _scrollRect.content.GetComponentsInChildren(_selectables);
         }
     }
+
     void Awake()
     {
-        m_ScrollRect = GetComponent<ScrollRect>();
+        _scrollRect = GetComponent<ScrollRect>();
     }
+
     void Start()
     {
-        if (m_ScrollRect)
+        if (_scrollRect)
         {
-            m_ScrollRect.content.GetComponentsInChildren(m_Selectables);
-            if(m_Selectables.Count > 0)
+            _scrollRect.content.GetComponentsInChildren(_selectables);
+            if(_selectables.Count > 0)
 			{
-                EventSystem.current.firstSelectedGameObject = m_Selectables[0].gameObject;
+                EventSystem.current.firstSelectedGameObject = _selectables[0].gameObject;
 			}
         }
         Setup();
-        //ScrollToSelected(true);
     }
+
     void Update()
     {
-        // Scroll via input.
-        //InputScroll();
         TriggerScroll();
-        if (!mouseOver)
-        {
-            // Lerp scrolling code.
-            //m_ScrollRect.normalizedPosition = Vector2.Lerp(m_ScrollRect.normalizedPosition, m_NextScrollPosition, scrollSpeed * Time.unscaledDeltaTime);
-        }
-        else
-        {
-            m_NextScrollPosition = m_ScrollRect.normalizedPosition;
-        }
-    }
-    void InputScroll()
-    {
-        if (m_Selectables.Count > 0)
-        {
-            //remove the rePlayer getaxis calls is you aren't using Rewired
-            //if it still doesn't work, check your input manager settings's axes and make sure they are defined properly
-            //if you're using the new input system, this is also probably where you should replace the calls to the old one
-            if (Input.GetAxis("Vertical") != 0.0f || Input.GetAxis("Horizontal") != 0.0f || Input.GetButtonDown("Horizontal")
-                || Input.GetButtonDown("Vertical") || Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
-            {
-                ScrollToSelected(false);
-            }
-        }
     }
 
     private void TriggerScroll()
@@ -80,90 +56,55 @@ public class ScrollRectAutoScroll : MonoBehaviour
         int selectedIndex = 0;
         if (selectedElement)
         {
-            selectedIndex = m_Selectables.IndexOf(selectedElement);
+            selectedIndex = _selectables.IndexOf(selectedElement);
         }
-        if (selectedIndex != lastSelected)
+        if (selectedIndex != _lastSelectedSlotIndex)
         {
-            var lastRow = Mathf.CeilToInt(lastSelected / 5);
+            var lastRow = Mathf.CeilToInt(_lastSelectedSlotIndex / 5);
             var selectedRow = Mathf.CeilToInt(selectedIndex / 5);
 
             if (selectedRow == lastRow) return;
 
-            int changeSign = selectedRow > lastRow ? -1 : 1;
+            //Debug.Log($"Changed from [slot {_lastSelectedSlotIndex} row {lastRow}] to [slot {selectedIndex} row {selectedRow}] position {_scrollRect.verticalNormalizedPosition} | visible {FirstVisibleRowIndex}-{LastVisibleRowIndex}");
 
-            var newValue = m_ScrollRect.verticalNormalizedPosition + (scrollMoveFraction * changeSign);
-            m_ScrollRect.verticalNormalizedPosition = Mathf.Clamp01(newValue);
+            if (selectedRow > lastRow && selectedRow > LastVisibleRowIndex - 1)
+			{
+                var newValue = _nextScrollPosition.y - _scrollMoveFraction;
+                _nextScrollPosition = new Vector2(0, Mathf.Clamp01(newValue));
+            } 
+            else if (selectedRow < lastRow && selectedRow < FirstVisibleRowIndex + 1)
+            {
+                var newValue = _nextScrollPosition.y + _scrollMoveFraction;
+				_nextScrollPosition = new Vector2(0, Mathf.Clamp01(newValue));
+			}
+            //Debug.Log($"Changed from [slot {lastSelected} row {lastRow}] to [slot {selectedIndex} row {selectedRow}] position {m_ScrollRect.verticalNormalizedPosition} | visible {FirstVisibleRowIndex}");
 
-            Debug.Log($"Changed from [{lastSelected} row {lastRow}] to [{selectedIndex} row {selectedRow}] position {m_ScrollRect.verticalNormalizedPosition}");
-
-            lastSelected = selectedIndex;
+			StartCoroutine(ScrollTo(_scrollRect.normalizedPosition, _nextScrollPosition, 0.1f));
+			_lastSelectedSlotIndex = selectedIndex;
         }
     }
 
-    void ScrollToSelected(bool quickScroll)
-    {
-        int selectedIndex = -1;
-        Selectable selectedElement = EventSystem.current.currentSelectedGameObject ? EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>() : null;
+	private IEnumerator ScrollTo(Vector2 startPos, Vector2 endPos, float time)
+	{
+		float i = 0.0f;
+		float rate = 1.0f / time;
+		while (i < 1.0)
+		{
+			yield return new WaitForEndOfFrame();
+			i += (Time.deltaTime * rate);
+            _scrollRect.normalizedPosition = Vector2.Lerp(startPos, endPos, i);
+		}
+	}
 
-        int selectedRow = -1;
-
-        if (selectedElement)
-        {
-            selectedIndex = m_Selectables.IndexOf(selectedElement);
-            selectedRow = Mathf.CeilToInt(selectedIndex / 5);
-        }
-        if (selectedRow > -1)
-        {
-
-            //int v = selectedRow > lastRow ? 1 : -1;
-            //var clmp = Mathf.Clamp(m_ScrollRect.verticalNormalizedPosition + (scrollMoveFraction * v), 0f, 1f);
-
-            //if (quickScroll)
-            //{
-            //    m_ScrollRect.normalizedPosition = new Vector2(0, clmp);
-            //    m_NextScrollPosition = m_ScrollRect.normalizedPosition;
-            //}
-            //else
-            //{
-            //    m_NextScrollPosition = new Vector2(0, clmp);
-            //    Debug.Log($"{m_ScrollRect.verticalNormalizedPosition}");
-            //}
-
-            //lastRow = selectedRow;
-
-            // first
-            //if (quickScroll)
-            //{
-            //    m_ScrollRect.normalizedPosition = new Vector2(0, 1 - (selectedRow / ((float)rows)));
-            //    m_NextScrollPosition = m_ScrollRect.normalizedPosition;
-            //}
-            //else
-            //{
-            //    m_NextScrollPosition = new Vector2(0, 1 - (selectedRow / ((float)rows)));
-            //    Debug.Log($"{m_ScrollRect.verticalNormalizedPosition}");
-            //}
-
-            //original
-            //if (quickScroll)
-            //{
-            //    m_ScrollRect.normalizedPosition = new Vector2(0, 1 - (selectedIndex / ((float)m_Selectables.Count - 1)));
-            //    m_NextScrollPosition = m_ScrollRect.normalizedPosition;
-            //}
-            //else
-            //{
-            //    m_NextScrollPosition = new Vector2(0, 1 - (selectedIndex / ((float)m_Selectables.Count - 1)));
-            //}
-        }
-    }
+	private int FirstVisibleRowIndex => (int)((1f - _nextScrollPosition.y) / _scrollMoveFraction);
+    private int LastVisibleRowIndex => FirstVisibleRowIndex + _rows - 1;
 
     private void Setup()
 	{
-        var child = m_Selectables.Count;
-        var step1 = Mathf.Max(child - 25, 0);
-        var step2 = step1 / 5;
-        rows = Mathf.CeilToInt(step2);
-
-        scrollMoveFraction = 1f / rows;
-        Debug.Log($"{child} -> {step1} -> {step2} -> {rows} -> {scrollMoveFraction}");
+        var childCount = _selectables.Count;
+        var hiddenSlots = Mathf.Max(childCount - _rows * _coulums, 0);
+        _totalRows = Mathf.CeilToInt(hiddenSlots / 5);
+        _scrollMoveFraction = 1f / _totalRows;
+        //Debug.Log($"{childCount} -> {hiddenSlots} -> {hiddenSlots / 5} -> {_totalRows} -> {_scrollMoveFraction}");
     }
 }
